eeba7fdd83bfd78fc37a2d54e0ba4924547b0f23
isidrovera <118472736+isidrovera@users.noreply.github.com>
2026-05-12 14:26:02 -0500

    ..

--- models/partner.py
+++ models/partner.py
@@ -67,6 +67,15 @@ class CustomResPartner(models.Model):
         string='Máquinas Compradas',
         compute='_compute_copier_machine_count'
     )
+    portal_empresa_ids = fields.Many2many(
+        'res.partner',
+        'res_partner_portal_empresa_rel',
+        'contacto_id',
+        'empresa_id',
+        string='Empresas visibles en portal',
+        domain=[('is_company', '=', True)],
+        help='Empresas adicionales cuyos equipos podrá ver este contacto en el portal.'
+    )
 
     def _compute_copier_order_count(self):
         """Contar órdenes que contienen máquinas usadas"""
